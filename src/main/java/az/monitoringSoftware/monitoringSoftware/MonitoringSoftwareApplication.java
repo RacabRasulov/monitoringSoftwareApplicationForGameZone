240e29e06d79e402711ac28cc081a234c9d371d6
vusalkhn.rm@gmail.com <vusalkhn.rm@gmail.com>
2023-08-06 10:32:50 +0400

    application added

---
+++ src/main/java/az/monitoringSoftware/monitoringSoftware/MonitoringSoftwareApplication.java
@@ -0,0 +1,13 @@
+package az.monitoringSoftware.monitoringSoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/monitoringSoftware/monitoringSoftware/MonitoringSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package az.monitoringSoftware.monitoringSoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
