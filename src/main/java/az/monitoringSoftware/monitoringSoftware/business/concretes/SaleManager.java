8dc1a2770ed2ce2b82c72f389df578e797f103cf
vusalkhn.rm@gmail.com <vusalkhn.rm@gmail.com>
2023-08-26 20:12:12 +0400

    creating sale bug fixed

--- src/main/java/az/monitoringSoftware/monitoringSoftware/business/concretes/SaleManager.java
+++ src/main/java/az/monitoringSoftware/monitoringSoftware/business/concretes/SaleManager.java
@@ -2,24 +2,28 @@ package az.monitoringSoftware.monitoringSoftware.business.concretes;
 
 import az.monitoringSoftware.monitoringSoftware.business.abstracts.SaleService;
 import az.monitoringSoftware.monitoringSoftware.business.requests.sale.CreateSaleRequest;
-import az.monitoringSoftware.monitoringSoftware.business.requests.sale.GetSaleByDeskIdRequest;
+import az.monitoringSoftware.monitoringSoftware.business.requests.saleProduct.CreatSaleProductRequest;
 import az.monitoringSoftware.monitoringSoftware.core.utilities.mappers.ModelMapperManager;
 import az.monitoringSoftware.monitoringSoftware.dataAccess.abstracts.DeskRepository;
+import az.monitoringSoftware.monitoringSoftware.dataAccess.abstracts.ProductRepository;
 import az.monitoringSoftware.monitoringSoftware.dataAccess.abstracts.SaleRepository;
 import az.monitoringSoftware.monitoringSoftware.domain.entities.Desk;
 import az.monitoringSoftware.monitoringSoftware.domain.entities.Sale;
+import az.monitoringSoftware.monitoringSoftware.domain.entities.SaleProduct;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class SaleManager implements SaleService {
-    public final SaleRepository saleRepository;
-    public final ModelMapperManager modelMapperManager;
-    public final DeskRepository deskRepository;
+    private final SaleRepository saleRepository;
+    private final ModelMapperManager modelMapperManager;
+    private final DeskRepository deskRepository;
+    private final ProductRepository productRepository;
 
 
     @Override
@@ -27,28 +31,27 @@ public class SaleManager implements SaleService {
         Optional<Desk> desk = deskRepository.findById(UUID.fromString(String
                 .valueOf(createSaleRequest.getDeskId())));
 
+        Sale sale = new Sale();
 
-        Sale sale = modelMapperManager.forRequest()
-                .map(createSaleRequest, Sale.class);
+        for(CreatSaleProductRequest createSaleProduct : createSaleRequest.getSaleProducts()){
+            SaleProduct saleProduct = new SaleProduct();
+            saleProduct.setProductId(createSaleProduct.getProductId());
+            saleProduct.setName(createSaleProduct.getName());
+            saleProduct.setPrice(createSaleProduct.getPrice());
+            saleProduct.setOrderCount(createSaleProduct.getOrderCount());
+            sale.getSaleProducts().add(saleProduct);
+        }
 
+        sale.setDeskId(createSaleRequest.getDeskId());
         sale.setDeskName(desk.get().getName());
         sale.setDeviceId(desk.get().getDevice().getId());
         sale.setDeviceName(desk.get().getDevice().getName());
         sale.setDevicePrice(desk.get().getDevice().getPrice());
-
+        for(SaleProduct product : sale.getSaleProducts()){
+            product.setSale(sale);
+        }
         saleRepository.save(sale);
 
     }
-
-    @Override
-    public GetSaleByDeskIdRequest getSaleByDeskIdRequest(UUID id) {
-
-        return modelMapperManager.forResponse()
-                .map(saleRepository.findByDeskId(
-                                UUID.fromString(String.valueOf((id))))
-                        , GetSaleByDeskIdRequest.class);
-
-    }
-
 
 }
