c24cf41896e49fbbba0bce221555f3d8763d6917
vusalkhn.rm@gmail.com <vusalkhn.rm@gmail.com>
2023-08-27 20:48:44 +0400

    commit

--- src/main/java/az/monitoringSoftware/monitoringSoftware/domain/entities/Product.java
+++ src/main/java/az/monitoringSoftware/monitoringSoftware/domain/entities/Product.java
@@ -1,6 +1,7 @@
 package az.monitoringSoftware.monitoringSoftware.domain.entities;
 
 import az.monitoringSoftware.monitoringSoftware.domain.common.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
